c44c38b0f76aea06eb535b2446088cc70de90011
CvProQuebec.ca <gerald@cvproquebec.ca>
2025-08-18 16:02:39 -0400

    Fix: Firebase variables configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './features/retirement/hooks/useLanguage';
+import { AuthProvider } from './hooks/useAuth';
 import Layout from './components/layout/Layout';
 
 // Pages de retraite
@@ -17,39 +18,41 @@ import RetirementReportsEn from './pages/RetirementReportsEn';
 
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            {/* Page d'accueil */}
-            <Route path="/" element={<RetraiteEntreeFr />} />
-            <Route path="/en" element={<RetraiteEntreeEn />} />
-            
-            {/* Routes d'entrée - Module Retirement */}
-            <Route path="/fr/retraite-entree" element={<RetraiteEntreeFr />} />
-            <Route path="/en/retirement-entry" element={<RetraiteEntreeEn />} />
-            
-            {/* Routes du module complet - Module Retirement */}
-            <Route path="/fr/retraite" element={<RetraiteFr />} />
-            <Route path="/en/retirement" element={<RetraiteEn />} />
-            
-            <Route path="/fr/retraite-module" element={<RetraiteModuleFr />} />
-            <Route path="/en/retirement-module" element={<RetraiteModuleEn />} />
-            
-            {/* Routes pour le module retraite Phase 1 */}
-            <Route path="/fr/retraite-module-phase1" element={<RetraiteModulePhase1Fr />} />
-            <Route path="/en/retirement-module-phase1" element={<RetraiteModulePhase1En />} />
-            
-            {/* Routes Rapports de Retraite */}
-            <Route path="/fr/rapports-retraite" element={<RapportsRetraiteFr />} />
-            <Route path="/en/retirement-reports" element={<RetirementReportsEn />} />
-            
-            {/* Redirection par défaut */}
-            <Route path="*" element={<RetraiteEntreeFr />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </LanguageProvider>
+    <AuthProvider>
+      <LanguageProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              {/* Page d'accueil */}
+              <Route path="/" element={<RetraiteEntreeFr />} />
+              <Route path="/en" element={<RetraiteEntreeEn />} />
+              
+              {/* Routes d'entrée - Module Retirement */}
+              <Route path="/fr/retraite-entree" element={<RetraiteEntreeFr />} />
+              <Route path="/en/retirement-entry" element={<RetraiteEntreeEn />} />
+              
+              {/* Routes du module complet - Module Retirement */}
+              <Route path="/fr/retraite" element={<RetraiteFr />} />
+              <Route path="/en/retirement" element={<RetraiteEn />} />
+              
+              <Route path="/fr/retraite-module" element={<RetraiteModuleFr />} />
+              <Route path="/en/retirement-module" element={<RetraiteModuleEn />} />
+              
+              {/* Routes pour le module retraite Phase 1 */}
+              <Route path="/fr/retraite-module-phase1" element={<RetraiteModulePhase1Fr />} />
+              <Route path="/en/retirement-module-phase1" element={<RetraiteModulePhase1En />} />
+              
+              {/* Routes Rapports de Retraite */}
+              <Route path="/fr/rapports-retraite" element={<RapportsRetraiteFr />} />
+              <Route path="/en/retirement-reports" element={<RetirementReportsEn />} />
+              
+              {/* Redirection par défaut */}
+              <Route path="*" element={<RetraiteEntreeFr />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </LanguageProvider>
+    </AuthProvider>
   );
 }
 
